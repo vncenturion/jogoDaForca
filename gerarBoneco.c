199c2111769aaa7e8158c677d72b0a74b94de42f
Vinicius Centurion <43175480+vncenturion@users.noreply.github.com>
2020-11-16 22:06:58 -0300

    figuras
    figuras da forca conforme erros, jogador vencedor e jogador eliminado

--- gerarBoneco.c
+++ gerarBoneco.c
@@ -1,10 +1,155 @@
 #include <stdio.h>
-#include <string.h>
-
 
 int main(void) 
 {
-  	printf
-	return 0;
-    
+  
+	printf("\nParabéns, JOGADOR X ganhou!\n\n");
+	printf("       ___________      \n");
+	printf("      '.=========.'     \n");
+	printf("      .-\\:      /-.    \n");
+	printf("     |( |:.     | )|    \n");
+	printf("      '-|:.     |-'     \n");
+	printf("        \\::.    /      \n");
+	printf("         '::. .'        \n");
+	printf("           ) (          \n");
+	printf("         _.' '._        \n");
+	printf("        '======='       \n\n");
+
+	printf("\nPuxa, JOGADOR X foi eliminado!\n\n");
+	printf("    .--------------.         \n");
+	printf("   /                \\      \n");
+	printf("  /                  \\    \n");
+	printf(" |   XXXX     XXXX   |     \n");
+	printf(" |   XXXX     XXXX   |      \n");
+	printf(" |   XXX       XXX   |      \n");
+	printf(" |                   |      \n");
+	printf(" \\__      XXX      __/     \n");
+	printf("   |\\     XXX     /|       \n");
+	printf("   | |           | |        \n");
+	printf("   | |[][][][][][] |        \n");
+	printf("    \\             _/       \n");
+	printf("     \\___________/         \n\n");
+
+
+	printf("DESENHO DA FORCA 6\n\n");
+	printf("xxxxxxxxxxxx\n");
+	printf("||  //     |\n");
+	printf("|| //     ( )\n");
+	printf("||//      _|_\n");
+	printf("||/      /[X]\\\n");
+	printf("||      / [X] \\\n");
+	printf("||        ( )\n");
+	printf("||       /   \\\n");
+	printf("||      /     \\\n");
+	printf("||              \n");
+	printf("||_______________.\n");
+	printf("|| 1 2 3 4 5 6 7 |\n");
+	printf("||XXXXXXXXXXXXXXX|\n\n");
+
+	printf("DESENHO DA FORCA 6\n\n");
+	printf("xxxxxxxxxxxx\n");
+	printf("||  //     |\n");
+	printf("|| //     ( )\n");
+	printf("||//      _|_\n");
+	printf("||/      /[X]\\\n");
+	printf("||      / [X] \\\n");
+	printf("||        ( )\n");
+	printf("||       /   \n");
+	printf("||      /    \n");
+	printf("||           \n");
+	printf("||_______________.\n");
+	printf("|| 1 2 3 4 5 6   |\n");
+	printf("||XXXXXXXXXXXXXXX|\n\n");
+
+	printf("DESENHO DA FORCA 5\n\n");
+	printf("xxxxxxxxxxxx\n");
+	printf("||  //     |\n");
+	printf("|| //     ( )\n");
+	printf("||//      _|_\n");
+	printf("||/      /[X]\\\n");
+	printf("||      / [X] \\\n");
+	printf("||        ( )\n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||_______________.\n");
+	printf("|| 1 2 3 4 5     |\n");
+	printf("||XXXXXXXXXXXXXXX|\n\n");
+
+	printf("DESENHO DA FORCA 4\n\n");
+	printf("xxxxxxxxxxxx\n");
+	printf("||  //     |\n");
+	printf("|| //     ( )\n");
+	printf("||//      _|_\n");
+	printf("||/      /[X]\\\n");
+	printf("||      / [X] \\\n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||_______________.\n");
+	printf("|| 1 2 3 4       |\n");
+	printf("||XXXXXXXXXXXXXXX|\n\n");
+
+	printf("DESENHO DA FORCA 3\n\n");
+	printf("xxxxxxxxxxxx\n");
+	printf("||  //     |\n");
+	printf("|| //     ( )\n");
+	printf("||//      _|_\n");
+	printf("||/      /[X]\n");
+	printf("||      / [X]\n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||_______________.\n");
+	printf("|| 1 2 3         |\n");
+	printf("||XXXXXXXXXXXXXXX|\n\n");
+
+	printf("DESENHO DA FORCA 2\n\n");
+	printf("xxxxxxxxxxxx\n");
+	printf("||  //     |\n");
+	printf("|| //     ( )\n");
+	printf("||//      _|_\n");
+	printf("||/       [X]\n");
+	printf("||        [X]\n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||_______________.\n");
+	printf("|| 1 2           |\n");
+	printf("||XXXXXXXXXXXXXXX|\n\n");
+
+	printf("DESENHO DA FORCA 1\n\n");
+	printf("xxxxxxxxxxxx\n");
+	printf("||  //     |\n");
+	printf("|| //     ( )\n");
+	printf("||//        \n");
+	printf("||/       \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||_______________.\n");
+	printf("|| 1             |\n");
+	printf("||XXXXXXXXXXXXXXX|\n\n");
+
+	printf("DESENHO DA FORCA 0\n\n");
+	printf("xxxxxxxxxxxx\n");
+	printf("||  //     |\n");
+	printf("|| //       \n");
+	printf("||//        \n");
+	printf("||/       \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||        \n");
+	printf("||_______________.\n");
+	printf("||               |\n");
+	printf("||XXXXXXXXXXXXXXX|\n\n");
+
+  	return 0;
 }
